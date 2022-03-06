d61e1a5a8020e33ee85abfc52b8340fec8c70cb7
= <=>
2022-03-07 03:05:51 +0530

    cart, login and register added

--- pizza-delivery-app/server.js
+++ pizza-delivery-app/server.js
@@ -5,18 +5,39 @@ const expressLayout = require("express-ejs-layouts")
 const path = require("path")
 const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) =>
-{
-    res.render("home")
-})
-
-//Set Assets
-app.use(express.static("public"))
 
 //set Template engine
 app.use(expressLayout)
 app.set("views", path.join(__dirname, "/resources/views"))
 app.set("view engine", "ejs")
+
+
+app.get("/", (req, res) =>
+{
+    res.render("home")
+})
+
+//cart page
+app.get("/cart", (req, res) =>
+{
+    res.render("customers/cart")
+})
+
+//login page
+app.get("/login", (req, res) =>
+{
+    res.render("auth/login")
+})
+
+//register page
+app.get("/register", (req, res) =>
+{
+    res.render("auth/register")
+})
+
+//Set Assets
+app.use(express.static("public"))
+
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
